9c6319ae9a768af8a9feccb0b06abc06557a3715
Leandru Martin <leandrumartin@gmail.com>
2026-02-06 11:56:26 -0600

    Handle bad input

--- ide-selector.py
+++ ide-selector.py
@@ -13,6 +13,27 @@ with open(ide_file, "r") as file:
     for line in file:
         editors.append(line.strip())
 
+def parse_selection(selection):
+    """Parse user selection to determine the chosen editor."""
+    def handle_index_error():
+        return parse_selection(input("Invalid selection. Please try again.\n").strip())
+
+    selected_editor = None
+    try:
+        # Interpret selection as an index
+        selected_editor = editors[int(selection) - 1]
+    except ValueError:
+        # Interpret selection as a substring
+        filtered = filter(lambda x: selection in x, editors)
+        filtered_list = list(filtered)
+        try:
+            selected_editor = filtered_list[0]
+        except IndexError:
+            selected_editor = handle_index_error()
+    except IndexError:
+        selected_editor = handle_index_error()
+    return selected_editor
+
 command = None
 if len(editors) == 1:
     command = f"{editors[0]} {path}"
@@ -21,15 +42,7 @@ else:
     for i, editor in enumerate(editors):
         print(f"\t{i + 1}. {editor}")
     selection = input("Choose which editor you would like to use. Enter the number or part of the name.\n").strip()
-    
-    selected_editor = None
-    try:
-        selected_editor = editors[int(selection) - 1]
-    except:
-        filtered = filter(lambda x: selection in x, editors)
-        filtered_list = list(filtered)
-        selected_editor = filtered_list[0]
-    
+    selected_editor = parse_selection(selection)   
     command = f"{selected_editor} {path}"
 print(command)
 os.system(command)
